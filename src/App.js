4eea801614f8637a45ce33b1b2e327afe2419f37
Arturo Zamora <arturo-oz@hotmail.com>
2018-11-26 11:55:27 -0700

    Test

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React Again!
+            Lol!
           </a>
         </header>
       </div>
